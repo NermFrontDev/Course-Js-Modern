f8f9d8dd287eb61085b4d9282aa98c43cdd2dd5b
NRamirez <nerm.animator@gmail.com>
2024-04-28 13:31:26 -0600

    Variables and comments

---
+++ 01-Fundamentos/app.js
@@ -0,0 +1,12 @@
+
+// console.log('Hola mundo');
+
+// Se puede reasignar sin simportar el scope
+var a = 10; // Ya no se usa actualmente
+// Reasignable dependiendo el scope
+let b = 10;
+// No se puede reasignar la variable, sin importar el scope
+const c = 10;
+
+// c = 20 // no se puede reasignar
+
