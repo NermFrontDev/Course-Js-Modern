ceaab35a1128f5dc02de7cc9e5ec41c4ca01e589
NRamirez <nerm.animator@gmail.com>
2024-04-29 09:06:40 -0600

    Palabras reservadas y nombre de variables

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,25 @@
+
+
+let constVar = 123;
+
+// const
+// if
+// try
+// while
+// for()
+// enum
+
+
+// se puede inicializar variables con guion bajo
+let _variableName = 123;
+
+// underscore para separar numeros o texto
+let precio_99_99 = 123;
+
+// camelCase
+let jugadorConPuntajeMasAlto = 'Nerm Dev';
+
+// UppercamelCase
+class JuegoPuntos {
+
+}
