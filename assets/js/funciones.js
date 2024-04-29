f5336a400b8cbbae1d3003d45bb7d2c25ac0892a
NRamirez <nerm.animator@gmail.com>
2024-04-29 11:32:10 -0600

    Retorno de Funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,7 +1,12 @@
 // Funcion tradicional:
+// Si la funcion no contiene un return, lo que devuelve es undefined
 function saludar( nombre ) {
     console.log( arguments );
     console.log('Hola ' + nombre);
+    return 1;
+
+    // Esto nunca se va a ejecutar
+    console.log('Soy una nueva linea de código después del return');
 }
 
 // Funcion anónima:
@@ -19,8 +24,34 @@ const saludarFlecha2 = ( nombre ) => {
 }
 
 
-saludar('Nerm', 40, true, 'Honduras');
+const retornoSaludar = saludar('Nerm', 40, true, 'Honduras');
+console.log( retornoSaludar );
 saludarFlecha();
 saludarFlecha2('Nerm Developer');
 // saludar2('Nerm');
 
+
+
+/* ============= Ejercicios ============= */
+
+function sumar(a, b) {
+    return a + b;
+}
+
+
+// const sumar2 = (a,b) => {
+//     return a + b;
+// }
+
+const sumar2 = (a,b) => a + b;
+
+console.log( sumar2(1, 2) );
+
+/* Ejercicio de clase */
+function getAleatorio() {
+    return Math.random();
+}
+
+// getAleatorio en funcion de flecha
+const getAleatorio2 = () => Math.random();
+console.log( getAleatorio2() );
