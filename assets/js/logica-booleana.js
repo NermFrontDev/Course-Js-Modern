719aee315eb3ac19a5bddf2dea313df28bffac45
NRamirez <nerm.animator@gmail.com>
2024-04-30 08:05:52 -0600

    Pro tip: Asignaciones con operadores

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -34,4 +34,21 @@ console.log( false || false ); // false
 console.log( regresaTrue() || regresaFalse() ); // true
 
 
+console.warn('Asignaciones');
+
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola a1' && 150;
+const a2 = false || 'Hola a2' || 100;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo';
+
+console.log({ a1, a2, a3, a4 });
+
+
+
+
 
