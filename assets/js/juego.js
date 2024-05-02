e2b556d86db38a1ac19987f22a4102b29d7aa965
NRamirez <nerm.animator@gmail.com>
2024-05-02 10:16:16 -0600

    Optimizaciones - Tercera parte

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,7 +1,7 @@
 
 // Patrón Módulo
 
-(() => {
+const miModulo = (() => {
     'use strict'
 
     let deck         = [];
@@ -21,10 +21,16 @@
     // Inicializa el juego
     const inicializarJuego = ( numJugadores = 2 ) => {
         deck = crearDeck();
+        puntosJugadores = [];
         for (let i = 0; i < numJugadores; i++) {
             puntosJugadores.push(0);
-            
         }
+
+        aumentarPuntos.forEach( elem => elem.innerText = 0 );
+        divCartasJugadores.forEach( elem => elem.innerHTML = '' );
+    
+        btnPedir.disabled = false;
+        btnStop.disabled = false;
     }
     
     // Esta funcion crea nueva baraja
@@ -78,30 +84,12 @@
             imgCarta.classList.add('carta');
             divCartasJugadores[turno].append( imgCarta );
     }
-    
-    // Turno de la computadora
-    const turnoComputadora = ( puntosMinimos ) => {
 
-        let puntosComputadora = 0;
+    const definirGanador = () => {
 
-        do {
-            const carta = pedirCarta();
-        
-            puntosComputadora = acumularPuntos( carta, puntosJugadores.length - 1 );
+        // Desestructuración de arreglos
+        const [ puntosMinimos, puntosComputadora ] = puntosJugadores;
 
-            crearCarta( carta, puntosJugadores.length - 1 );
-            
-            // const imgCarta = document.createElement('img');
-            // imgCarta.src = `assets/images/cartas/${ carta }.png`;
-            // imgCarta.classList.add('carta');
-            // computadoraCartas.append( imgCarta );
-    
-            if ( puntosMinimos > 21 ) {
-                break;
-            }
-            
-        } while ( (puntosComputadora < puntosMinimos) && ( puntosMinimos <= 21 ) );
-    
         setTimeout(() => {
             if ( puntosMinimos === puntosComputadora ) {
                 alert('Nadie gana');
@@ -114,6 +102,22 @@
             }
 
         }, 15);
+    }
+    
+    // Turno de la computadora
+    const turnoComputadora = ( puntosMinimos ) => {
+
+        let puntosComputadora = 0;
+
+        do {
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos( carta, puntosJugadores.length - 1 );
+            crearCarta( carta, puntosJugadores.length - 1 );
+            
+        } while ( (puntosComputadora < puntosMinimos) && ( puntosMinimos <= 21 ) );
+
+        definirGanador();
+    
     }
     
     
@@ -152,17 +156,10 @@
 
         inicializarJuego();
 
-        // deck = [];
-        // deck = crearDeck();
-    
-        // aumentarPuntos[0].innerText = 0;
-        // aumentarPuntos[1].innerText = 0;
-    
-        // jugadorCartas.innerHTML = '';
-        // computadoraCartas.innerHTML = '';
-    
-        // btnPedir.disabled = false;
-        // btnStop.disabled = false;
     })
+
+    return {
+        nuevoJuego: inicializarJuego
+    }
     
 })();
