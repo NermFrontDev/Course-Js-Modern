da9cd5dcbe4dd02188355907ee8759fa1b69b535
NRamirez <nerm.animator@gmail.com>
2024-05-03 07:47:17 -0600

    Métodos en las clases

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -14,10 +14,22 @@ class Persona {
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    quienSoy() {
+        console.log( `Soy ${this.nombre} y mi identidad es ${this.codigo}` );
+    }
+
+    miFrase() {
+        // this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
 }
 
 // La palabra reservada 'new' indica a Js que se crea una nueva instancia
 const spiderpapu = new Persona( 'Peterpapu', 'Spiderpapu', 'Spiderpapu al rescate!! mmm uaah!' );
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman' );
-console.log( spiderpapu );
-console.log( ironman );
+// console.log( spiderpapu );
+// console.log( ironman );
+
+spiderpapu.quienSoy();
+spiderpapu.miFrase();
