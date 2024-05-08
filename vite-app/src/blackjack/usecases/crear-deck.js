e8bfbd4865962042aac0b30003383d679567936b
NRamirez <nerm.animator@gmail.com>
2024-05-08 08:09:49 -0600

    Mejorar la documentación JSDoc Comments

--- vite-app/src/blackjack/usecases/crear-deck.js
+++ vite-app/src/blackjack/usecases/crear-deck.js
@@ -1,7 +1,20 @@
 import _ from 'underscore';
 
-// Esta función crea un nuevo deck
+
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} regresa un nuevo deck de cartas
+ */
+
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if ( !tiposDeCarta || tiposDeCarta.length === 0 ) 
+        throw new Error ('TiposDeCarta es obligatorio como arreglo de string');
+    
+    if ( !tiposEspeciales || tiposEspeciales.length === 0 ) 
+        throw new Error ('tiposEspeciales es obligatorio como arreglo de string');
 
     let deck = [];
 
