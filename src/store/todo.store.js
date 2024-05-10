47c7bf9ccf9e4dcfa87ca502e7016f3e8baa79c0
NRamirez <nerm.animator@gmail.com>
2024-05-10 09:30:09 -0600

    Implementar métodos del Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -24,12 +24,34 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All:
+            
+            return [...state.todos];
+    
+        case Filters.Completed:
+
+            return state.filter( todo => todo.done );
+        
+        case Filters.Pending:
+
+            return state.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+
+    }
+}
+
 /**
  * 
  * @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not implemented');
+    if( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo( description ) );
 }
 
 const toggleTodo = ( todoId ) => {
@@ -37,19 +59,23 @@ const toggleTodo = ( todoId ) => {
 }
 
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done );
 }
 
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const setFilter = ( newFilter = Filters.All ) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default {
@@ -57,6 +83,7 @@ export default {
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
