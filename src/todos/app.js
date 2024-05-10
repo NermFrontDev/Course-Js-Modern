1fda2bb9f5ef732bc97da4d50cb5bbdf79f4aece
NRamirez <nerm.animator@gmail.com>
2024-05-10 10:08:13 -0600

    Renderizar lista de Todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,10 @@
 import html from "./app.html?raw";
+import todoStore from "../store/todo.store";
+import { renderTodos } from "./uses-cases";
+
+const elementIDs = {
+    TodoList: '.todo-list'
+}
 
 /**
  * 
@@ -6,13 +12,18 @@ import html from "./app.html?raw";
  */
 export const App = ( elementId ) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos( elementIDs.TodoList, todos );
+    }
+
     // Cuando la función App() se llama
     (() => {
 
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append( app );
-
+        displayTodos();
     })();
 
 
