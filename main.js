ab9e6a62aa1da05f019f6335c21f683f41977ff7
NRamirez <nerm.animator@gmail.com>
2024-05-14 12:47:55 -0600

    Promise - Race

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { environmentComponent } from './src/concepts/01-environments';
 import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
+import { promiseRaceComponent } from './src/concepts/04-promise-race';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -23,4 +24,5 @@ document.querySelector('#app').innerHTML = `
 const element = document.querySelector('.card');
 // environmentComponent( element );
 // callbacksComponent(element);
-promiseComponent(element);
+// promiseComponent(element);
+promiseRaceComponent(element);
