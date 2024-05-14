f7ecbb1d2ee7d253da2b261c4effa97fea5f1dfd
NRamirez <nerm.animator@gmail.com>
2024-05-14 11:17:43 -0600

    Utilizar una promesa

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -7,7 +7,22 @@ import {heroes} from '../data/heroes';
  */
 export const promiseComponent = ( element ) => {
 
+    const hero1 = '5d86371fd55e2e2a30fe1cc3';
+    const hero2 = '5d86371fd55e2e2a30fe1ccb';
+
+    const renderHero = ( hero ) => {
+        element.innerHTML = hero.name;
+    }
+
+    const renderError = (error) => {
+        element.innerHTML = `
+            <h3>${error}</h3>
+        `;
+    }
     
+    findHero( hero1 )
+    .then( superHero => renderHero( superHero ) )
+    .catch( error => renderError(error) );
     
 }
 
