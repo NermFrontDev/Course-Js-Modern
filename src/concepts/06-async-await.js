e8068e42041302225d0701afc9463400c4edfd08
NRamirez <nerm.animator@gmail.com>
2024-05-14 18:31:38 -0600

    Errores en Async - Await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -8,10 +8,15 @@ export const asyncAwaitComponent = async ( element ) => {
     const id1 = '5d86371fd55e2e2a30fe1ccb';
     const id2 = '5d86371fd55e2e2a30fe1ccb2';
 
-    const hero1 = await findHero( id1 );
-    const hero2 = await findHero( id2 );
-    
-    element.innerHTML = `${hero1.name} - ${hero2.name}`;
+    try {
+        const hero1 = await findHero( id1 );
+        const hero2 = await findHero( id2 );
+        
+        element.innerHTML = `${hero1.name} - ${hero2.name}`;
+    } catch (error) {
+        element.innerHTML = error;
+    }
+
 }
 
 const findHero = async( id ) => {
