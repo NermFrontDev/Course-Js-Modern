3cd24685eb59025a4ddb565bf917ea9fcbe60066
NRamirez <nerm.animator@gmail.com>
2024-05-16 12:23:32 -0600

    FetchApi - Petición Http GET

--- src/ricky-morty/ricky-morty-app.js
+++ src/ricky-morty/ricky-morty-app.js
@@ -1,12 +1,28 @@
+const base_url = 'https://rickandmortyapi.com/api/'
+
+
+/**
+ * @returns {Promise<Object>} character information
+ */
+const fetchCharacter = async () => {
+    const resp = await fetch(`${base_url}character/1`);
+    const data = await resp.json();
+
+    console.log( data );
+    
+    return data;
+}
+
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export const RickyMortyApp = ( element ) => {
+export const RickyMortyApp = async ( element ) => {
 
     document.querySelector('#app-title').innerHTML = 'Ricky N Morty API';
-
     element.innerHTML = 'Loading...';
+    const character = await fetchCharacter();
+    element.innerHTML = 'Tenemos data!';
 
 }
