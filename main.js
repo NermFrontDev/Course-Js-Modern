cddbba8b6f117bbf2fda8ea2c4ca28504a3226a0
NRamirez <nerm.animator@gmail.com>
2024-05-16 13:57:57 -0600

    Tarea: Siguiente personaje - Agregué funcionalidades extra(imagen, btn anterior y condiciones)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { RickyMortyApp } from './src/ricky-morty/ricky-morty-app'
+import imgApi from "./public/api-logo.svg";
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,6 +11,9 @@ document.querySelector('#app').innerHTML = `
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${imgApi}" class="logo api" alt="Api logo" />
     </a>
     <h1 id="app-title">Hello Vite!</h1>
     <div class="card">
--- src/ricky-morty/ricky-morty-app.js
+++ src/ricky-morty/ricky-morty-app.js
@@ -1,11 +1,12 @@
 const base_url = 'https://rickandmortyapi.com/api/'
 
+let currentCharacterId = 1; // ID del personaje actual
 
 /**
  * @returns {Promise<Object>} character information
  */
 const fetchCharacter = async () => {
-    const resp = await fetch(`${base_url}character/1`);
+    const resp = await fetch(`${base_url}character/${currentCharacterId}`);
     const data = await resp.json();
 
     console.log( data );
@@ -25,16 +26,56 @@ export const RickyMortyApp = async ( element ) => {
 
     // const character = await fetchCharacter();
 
+    const loadingImage = document.createElement('img');
+    loadingImage.src = 'https://rickandmortyapi.com/api/character/avatar/19.jpeg'; // Ruta de la imagen de carga
+    loadingImage.classList.add('img-character-loading');
+
     const characterLabel = document.createElement('h3');
     const specieLabel = document.createElement('p');
+    const imgCharacter = document.createElement('img');
+    imgCharacter.classList.add('img-character');
+
     const nextCharacterBtn = document.createElement('button');
     nextCharacterBtn.innerText = ('Next Character');
+    nextCharacterBtn.classList.add('mx-2');
+
+    const prevCharacterBtn = document.createElement('button');
+    prevCharacterBtn.innerText = ('Previous Character');
+    prevCharacterBtn.classList.add('mx-2');
 
     const renderCharacter = ( data ) => {
+        if (currentCharacterId === 1) {
+            prevCharacterBtn.disabled = true;
+        } else {
+            prevCharacterBtn.disabled = false;
+        }
+        element.replaceChildren( prevCharacterBtn, nextCharacterBtn, characterLabel, specieLabel, imgCharacter );
         characterLabel.innerHTML = data.name;
         specieLabel.innerHTML = `I'm a ${data.species}`;
-        element.replaceChildren( characterLabel, specieLabel, nextCharacterBtn )
-    }
+
+        // Ocultar imagen de carga cuando la imagen real esté cargada
+        imgCharacter.onload = () => {
+            element.removeChild(loadingImage);
+        };
+
+        // Agregar imagen de carga antes de cargar la imagen real
+        element.insertBefore(loadingImage, imgCharacter);
+
+        imgCharacter.src = data.image;
+    };
+
+    nextCharacterBtn.addEventListener('click', async () => {
+        currentCharacterId++;
+        const character = await fetchCharacter();
+        renderCharacter( character );
+    });
+
+    prevCharacterBtn.addEventListener('click', async () => {
+            currentCharacterId--;
+            const character = await fetchCharacter();
+            renderCharacter( character );
+
+    });
 
     fetchCharacter()
         .then( renderCharacter );
